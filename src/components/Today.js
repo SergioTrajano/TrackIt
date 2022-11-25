303b5b0978235ccf03f893383197f9f7de596a07
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-11-25 00:34:54 -0300

    fix: adiciona as dependencias que faltavam do useEffect

--- src/components/Today.js
+++ src/components/Today.js
@@ -9,57 +9,64 @@ import PorcentageHabitsDoneToday from "../context/PorcentageHabitsDoneToday";
 import HabitsToday from "../context/HabitsToday";
 
 export default function Today() {
-
-    const {todayHabits, setTodayHabits} = useContext(HabitsToday);
+    const { todayHabits, setTodayHabits } = useContext(HabitsToday);
     const { account } = useContext(AccountContext);
     const { porcentageHabitsDoneToday, setPorcentageHabitsDoneToday } = useContext(PorcentageHabitsDoneToday);
-    const utc = require('dayjs/plugin/utc');
-    const updateLocale = require('dayjs/plugin/updateLocale');
-    
+    const utc = require("dayjs/plugin/utc");
+    const updateLocale = require("dayjs/plugin/updateLocale");
+
     dayjs.extend(utc);
     dayjs.extend(updateLocale);
-    dayjs.updateLocale('en', {
-        weekdays: [
-            "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"
-        ]
+    dayjs.updateLocale("en", {
+        weekdays: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"],
     });
 
     const date = dayjs.utc().local();
-    
+
     useEffect(() => {
         const config = {
             headers: {
-                Authorization: `Bearer ${account.token}`
-            }
+                Authorization: `Bearer ${account.token}`,
+            },
         };
         const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today", config);
-        promise.then(response => setTodayHabits(response.data));
-    }, [todayHabits]);
+        promise.then((response) => setTodayHabits(response.data));
+    }, [todayHabits, account.token, setTodayHabits]);
 
     useEffect(() => {
         if (todayHabits.length) {
-            setPorcentageHabitsDoneToday(todayHabits.filter( habit => habit.done === true).length / todayHabits.length);
+            setPorcentageHabitsDoneToday(
+                todayHabits.filter((habit) => habit.done === true).length / todayHabits.length
+            );
         } else {
             setPorcentageHabitsDoneToday(0);
         }
-    }, [todayHabits]);
-    
+    }, [todayHabits, setPorcentageHabitsDoneToday]);
+
     function habitsDoneTodayporcent() {
         if (porcentageHabitsDoneToday) {
-            return <PorcentageText>
-                        <p>
-                        {(porcentageHabitsDoneToday*100).toFixed(0)}% dos hábitos concluídos
-                        </p>
-                    </PorcentageText>;
+            return (
+                <PorcentageText>
+                    <p>{(porcentageHabitsDoneToday * 100).toFixed(0)}% dos hábitos concluídos</p>
+                </PorcentageText>
+            );
         }
         return <p>Nenhum hábito concluído ainda</p>;
     }
 
     function renderTodayshabits() {
         if (todayHabits.length) {
-            return todayHabits.map( (habit) => <TodayHabit key={habit.id} habitName={habit.name} habitId={habit.id} isDone={habit.done} currentSequence={habit.currentSequence} highestSequence={habit.highestSequence} />);
-        }
-        else return <p>Parece que você não tem hábitos para este dia da semana, que tal criar algum?</p>;
+            return todayHabits.map((habit) => (
+                <TodayHabit
+                    key={habit.id}
+                    habitName={habit.name}
+                    habitId={habit.id}
+                    isDone={habit.done}
+                    currentSequence={habit.currentSequence}
+                    highestSequence={habit.highestSequence}
+                />
+            ));
+        } else return <p>Parece que você não tem hábitos para este dia da semana, que tal criar algum?</p>;
     }
 
     const descripition = habitsDoneTodayporcent();
@@ -73,37 +80,37 @@ export default function Today() {
             </TodaysDescription>
             {listTodayHabits}
         </Container>
-    )
+    );
 }
 
 const Container = styled.div`
-    background-color: #F2F2F2;
+    background-color: #f2f2f2;
     min-height: 100vh;
     padding: 90px 4.53vw;
     box-sizing: border-box;
-    font-family: 'Lexend Deca', sans-serif;
-`
+    font-family: "Lexend Deca", sans-serif;
+`;
 
 const TodaysDescription = styled.div`
-    font-family: 'Lexend Deca', sans-serif;
+    font-family: "Lexend Deca", sans-serif;
     margin-bottom: 30px;
 
     > p:first-child {
-        color: #126BA5;
+        color: #126ba5;
         font-size: 6.13vw;
         line-height: 7.47vw;
     }
 
     > p:last-child {
-        color: #BABABA;
+        color: #bababa;
         font-size: 4.8vw;
         line-height: 5.87vw;
     }
-`
+`;
 const PorcentageText = styled.div`
     p {
-        color: #8FC549;
+        color: #8fc549;
         font-size: 4.8vw;
         line-height: 5.87vw;
     }
-`
+`;
