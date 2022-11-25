e907a13856f50aebe4f9edf529ca23c2f34a73dd
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-11-25 00:34:23 -0300

    fix: adiciona as dependencias que faltavam no useEffect hook

--- src/components/History.js
+++ src/components/History.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 import dayjs from "dayjs";
 import axios from "axios";
 import { useEffect, useState, useContext } from "react";
@@ -10,7 +10,6 @@ import "../CSS/calendar.css";
 import HabitClickedDay from "./HabitClickedDay";
 
 export default function History() {
-
     const [history, setHistory] = useState([]);
     const { account } = useContext(AccountContext);
     const [clickedDay, setClickedDay] = useState(<></>);
@@ -21,57 +20,65 @@ export default function History() {
     useEffect(() => {
         const config = {
             headers: {
-                Authorization: `Bearer ${account.token}`
-            }
+                Authorization: `Bearer ${account.token}`,
+            },
         };
-        const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily", config);
-        promise.then( response => setHistory(response.data));
-        promise.catch( () => alert("Erro no servidor!"));
-    }, []);
+        const promise = axios.get(
+            "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily",
+            config
+        );
+        promise.then((response) => setHistory(response.data));
+        promise.catch(() => alert("Erro no servidor!"));
+    }, [account.token]);
 
     function verifyDay({ date, view }) {
-        const element = history.find( day => day.day === dayjs(date).format("DD/MM/YYYY"));
-        
+        const element = history.find((day) => day.day === dayjs(date).format("DD/MM/YYYY"));
+
         if (element && view === "month" && element.day !== today) {
-            if (element.habits.filter( habit => !habit.done).length) return "not-all-done";
+            if (element.habits.filter((habit) => !habit.done).length) return "not-all-done";
             return "all-done";
         }
     }
 
-    function disableDay({ date, view}) {
+    function disableDay({ date, view }) {
         if (view === "month") {
-            if (history.find(day => day.day === dayjs(date).format("DD/MM/YYYY")) || dayjs(date).format("DD/MM/YYYY") === dayjs().format("DD/MM/YYYY")) {
+            if (
+                history.find((day) => day.day === dayjs(date).format("DD/MM/YYYY")) ||
+                dayjs(date).format("DD/MM/YYYY") === dayjs().format("DD/MM/YYYY")
+            ) {
                 return false;
-            }
-            else return true;
+            } else return true;
         }
     }
 
     function renderCalendar() {
         if (history.length) {
-            return  <Calendar 
-                        calendarType="US"
-                        tileClassName={verifyDay}
-                        tileDisabled={disableDay}
-                        onClickDay={(value) => renderClickedDayHabits(value)}
-                    />;
-        }
-        else return <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>;
+            return (
+                <Calendar
+                    calendarType="US"
+                    tileClassName={verifyDay}
+                    tileDisabled={disableDay}
+                    onClickDay={(value) => renderClickedDayHabits(value)}
+                />
+            );
+        } else return <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>;
     }
 
     function renderClickedDayHabits(value) {
-        const clickdDate = history.find( day => day.day === dayjs(value).format("DD/MM/YYYY"));
+        const clickdDate = history.find((day) => day.day === dayjs(value).format("DD/MM/YYYY"));
         setClickedDay(
             <ClickedDayHabitsContainer>
                 <p>{dayjs(value).format("dddd, DD/MM/YYYY")}</p>
-                <p>{Number(clickdDate.habits.filter(habit => habit.done).length*100/clickdDate.habits.length).toFixed(0)}% de hábitos concluidos</p>
-                {clickdDate.habits.map( (habit, i) => <HabitClickedDay 
-                                                    key={i}
-                                                    name={habit.name}
-                                                    wasDone={habit.done}
-                                                 />)}
+                <p>
+                    {Number(
+                        (clickdDate.habits.filter((habit) => habit.done).length * 100) / clickdDate.habits.length
+                    ).toFixed(0)}
+                    % de hábitos concluidos
+                </p>
+                {clickdDate.habits.map((habit, i) => (
+                    <HabitClickedDay key={i} name={habit.name} wasDone={habit.done} />
+                ))}
             </ClickedDayHabitsContainer>
-            
         );
     }
 
@@ -81,25 +88,25 @@ export default function History() {
             {showCalendar}
             {clickedDay}
         </Container>
-    )
+    );
 }
 
 const Container = styled.div`
     min-height: 100vh;
-    background-color: #F2F2F2;
+    background-color: #f2f2f2;
     padding: 70px 4.53vw 105px 4.53vw;
     box-sizing: border-box;
-    font-family: 'Lexend Deca', sans-serif;
+    font-family: "Lexend Deca", sans-serif;
 
     > p:first-child {
-        color: #126BA5;
+        color: #126ba5;
         font-size: 5.87vw;
         line-height: 7.47vw;
-        font-family: 'Lexend Deca', sans-serif;
+        font-family: "Lexend Deca", sans-serif;
         margin-top: 28px;
         margin-bottom: 17px;
     }
-`
+`;
 
 const ClickedDayHabitsContainer = styled.div`
     background-color: lightsteelblue;
@@ -110,7 +117,6 @@ const ClickedDayHabitsContainer = styled.div`
     align-items: center;
     padding: 13px;
     border-radius: 5px;
-    font-family: 'Lexend Deca', sans-serif;
+    font-family: "Lexend Deca", sans-serif;
     box-sizing: border-box;
-
-`
+`;
